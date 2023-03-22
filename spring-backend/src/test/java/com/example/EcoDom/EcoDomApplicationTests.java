dc1aeca4e7469489223ba43f7bdf2d8cca23b723
pabeler <pabeler@o2.pl>
2023-03-22 09:19:15 +0100

    Add Spring

---
+++ spring-backend/src/test/java/com/example/EcoDom/EcoDomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoDom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
